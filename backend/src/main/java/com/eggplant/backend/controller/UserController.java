88b302ec519aefd03a6429f823d7ac246da54e20
psjw <dlrudtn108@gmail.com>
2024-12-01 16:58:29 +0900

    JWT logout

--- backend/src/main/java/com/eggplant/backend/controller/UserController.java
+++ backend/src/main/java/com/eggplant/backend/controller/UserController.java
@@ -6,8 +6,12 @@ import com.eggplant.backend.jwt.JWTUtil;
 import com.eggplant.backend.service.CustomUserDetailsService;
 import com.eggplant.backend.service.UserService;
 import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -49,10 +53,22 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password) throws AuthenticationException {
-        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    public String login(@RequestParam String username, @RequestParam String password, HttpServletResponse response) throws AuthenticationException {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        return jwtUtil.generateToken(userDetails.getUsername());
+
+        String token = jwtUtil.generateToken(userDetails.getUsername());
+        // 쿠키 생성
+        ResponseCookie jwtCookie = ResponseCookie.from("onion_token", token)
+                .httpOnly(true) // 자바스크립트에서 접근 불가능
+//                .secure(true) // HTTPS에서만 전송
+                .path("/") // 모든 경로에서 유효
+                .maxAge(60 * 60) // 1시간 동안 유효
+                .sameSite("Strict") // CSRF 공격 방지
+                .build();
+        response.addHeader("Set-Cookie", jwtCookie.toString());
+        // 쿠키를 응답에 추가
+        return token;
     }
 
     @PostMapping("/token/validation")
@@ -61,5 +77,15 @@ public class UserController {
         if(!jwtUtil.validateToken(token)){
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Token is invalid.");
         }
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletResponse response) {
+        Cookie cookie = new Cookie("onion_token", null);
+        cookie.setHttpOnly(true);
+//        cookie.setSecure(true); //HTTPS에서만 전송
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
